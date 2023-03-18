a1575d08ee33c2e2b461a2de559f21578d097fc4
chiharakenta <chiharakenta0519@gmail.com>
2023-03-18 15:03:02 +0900

    style: AuthLayout

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,9 +1,32 @@
+import { SerializedStyles } from '@emotion/react';
+import { css, Interpolation } from '@emotion/react';
+import { Box } from '@mui/material';
+import { Container, SxProps } from '@mui/system';
 import { Outlet } from 'react-router-dom';
+import notionLogo from '../../assets/images/notion-logo.png';
 
 export const AuthLayout = () => {
+  const styles: { box: SxProps; img: SerializedStyles } = {
+    box: {
+      marginTop: 6,
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column'
+    },
+    img: css({
+      width: 100,
+      height: 100,
+      marginBottom: 3
+    })
+  };
   return (
     <div>
-      AuthLayout
+      <Container component="main" maxWidth="xs">
+        <Box sx={styles.box}>
+          <img src={notionLogo} alt="" css={styles.img} />
+          Notionクローン開発
+        </Box>
+      </Container>
       <Outlet />
     </div>
   );
