3390a2704473da6f58bd1f9c8569ca138fc56cf5
chiharakenta <chiharakenta0519@gmail.com>
2023-03-22 11:16:03 +0900

    feat: sidebar list items

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,5 +1,67 @@
+import { AddBoxOutlined, LogoutOutlined } from '@mui/icons-material';
+import { Box, Drawer, IconButton, List, ListItemButton, Typography } from '@mui/material';
 import { FC } from 'react';
 
 export const Sidebar: FC = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open={true}
+      sx={{ width: 250, height: '100vh' }}
+    >
+      <List sx={{ width: 250, height: '100vh' }}>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              marginBottom: '10px'
+            }}
+          >
+            <Typography variant="body2" fontWeight={700}>
+              chiharakenta
+            </Typography>
+            <IconButton>
+              <LogoutOutlined />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              marginBottom: '10px'
+            }}
+          >
+            <Typography variant="body2" fontWeight={700}>
+              お気に入り
+            </Typography>
+          </Box>
+        </ListItemButton>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between'
+            }}
+          >
+            <Typography variant="body2" fontWeight={700}>
+              プライベート
+            </Typography>
+            <IconButton>
+              <AddBoxOutlined fontSize="small" />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+      </List>
+    </Drawer>
+  );
 };
