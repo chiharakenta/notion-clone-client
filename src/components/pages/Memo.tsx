85d94f86c950ccaa70817a32345d1dccd78c3726
chiharakenta <chiharakenta0519@gmail.com>
2023-03-25 10:59:28 +0900

    feat: create Memo input, add star & delete icon

--- src/components/pages/Memo.tsx
+++ src/components/pages/Memo.tsx
@@ -1,5 +1,46 @@
+import { DeleteOutlined, StarBorderOutlined } from '@mui/icons-material';
+import { Box, IconButton, TextField } from '@mui/material';
 import { FC } from 'react';
 
 export const Memo: FC = () => {
-  return <div>Memo</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%'
+        }}
+      >
+        <IconButton>
+          <StarBorderOutlined />
+        </IconButton>
+        <IconButton color="error">
+          <DeleteOutlined />
+        </IconButton>
+      </Box>
+      <Box sx={{ padding: '10px 50px' }}>
+        <TextField
+          placeholder="無題"
+          variant="outlined"
+          fullWidth
+          sx={{
+            '.MuiOutlinedInput-input': { padding: 0, border: 'none' },
+            '.MuiOutlinedInput-notchedOutline': { border: 'none' },
+            '.MuiOutlinedInput-root': { fontSize: '2rem', fontWeight: 700 }
+          }}
+        />
+        <TextField
+          placeholder="追加"
+          variant="outlined"
+          fullWidth
+          sx={{
+            '.MuiOutlinedInput-input': { padding: 0, border: 'none' },
+            '.MuiOutlinedInput-notchedOutline': { border: 'none' },
+            '.MuiOutlinedInput-root': { fontSize: '1rem' }
+          }}
+        />
+      </Box>
+    </>
+  );
 };
